a2fd5f379aad65aa4f5fe7917e70c322f79ce142
Kevin Yang <yangkevint@gmail.com>
2022-08-12 13:48:06 -0500

    Finished http-hooks

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use( bodyParser.json() )
 
 app.use( ( req, res, next ) => {
+    
     res.setHeader( 'Access-Control-Allow-Origin', '*' );
     res.setHeader( 'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization' )
