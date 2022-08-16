5ddd7cd215642d8906cadd29c29f466b3fe16265
Kevin Yang <yangkevint@gmail.com>
2022-08-16 09:39:14 -0500

    Included Multer for uploading in backend

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,21 @@
 const express = require( 'express' );
 const { check } = require( 'express-validator' );
-
+const fileUpload = require( '../middleware/file-upload' )
 const usersControllers = require( '../controllers/users-controllers' );
 
 const router = express.Router();
 router.get( '/', usersControllers.getUsers );
-router.post( '/signup', [ check( "name" ).not().isEmpty(), check( 'email' ).normalizeEmail().isEmail(), check( 'password' ).isLength( { min: 6 } ) ], usersControllers.signup )
+router.post( '/signup',
+    fileUpload.single( 'image' ),
+    [ check( "name" )
+        .not()
+        .isEmpty(),
+    check( 'email' )
+        .normalizeEmail()
+        .isEmail(),
+    check( 'password' )
+        .isLength( { min: 6 } ) ],
+    usersControllers.signup )
 router.post( '/login', usersControllers.login )
 
 module.exports = router;
