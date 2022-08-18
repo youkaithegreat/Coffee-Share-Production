e44e484e5fd7039f4f916cb86bdce7758c2288c0
Kevin Yang <yangkevint@gmail.com>
2022-08-18 18:52:17 -0500

    Added in new env

---
+++ public/precache-manifest.78f60bf728913694d38726c4edb27c15.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9a86f7d1eddbb526a866dfec8d25aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91a26137003834f7fc2",
+    "url": "/static/css/3.16f68c43.chunk.css"
+  },
+  {
+    "revision": "a6486680558b90dea2c5",
+    "url": "/static/css/4.6b724ff8.chunk.css"
+  },
+  {
+    "revision": "ba9d966f0943c73c92c8",
+    "url": "/static/css/5.b0c10f8c.chunk.css"
+  },
+  {
+    "revision": "0866c9ed4d0aa90dbe5b",
+    "url": "/static/css/6.68399150.chunk.css"
+  },
+  {
+    "revision": "72a15cae82cd3e6ec614",
+    "url": "/static/css/7.09493055.chunk.css"
+  },
+  {
+    "revision": "401a448e60a940627e4c",
+    "url": "/static/css/main.6df5a7f4.chunk.css"
+  },
+  {
+    "revision": "d3d9c824863b3fc59aff",
+    "url": "/static/js/2.deeff388.chunk.js"
+  },
+  {
+    "revision": "f91a26137003834f7fc2",
+    "url": "/static/js/3.bf6b9820.chunk.js"
+  },
+  {
+    "revision": "a6486680558b90dea2c5",
+    "url": "/static/js/4.b66be395.chunk.js"
+  },
+  {
+    "revision": "ba9d966f0943c73c92c8",
+    "url": "/static/js/5.5cbecef4.chunk.js"
+  },
+  {
+    "revision": "0866c9ed4d0aa90dbe5b",
+    "url": "/static/js/6.186c334c.chunk.js"
+  },
+  {
+    "revision": "72a15cae82cd3e6ec614",
+    "url": "/static/js/7.b7a21a60.chunk.js"
+  },
+  {
+    "revision": "401a448e60a940627e4c",
+    "url": "/static/js/main.abfcec2c.chunk.js"
+  },
+  {
+    "revision": "873a937b6cc2e5b85bee",
+    "url": "/static/js/runtime-main.3785f9f2.js"
+  }
+]);
