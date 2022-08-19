d15942352802b7d9773c4c0c2d7b3146c3168a6e
Kevin Yang <yangkevint@gmail.com>
2022-08-19 13:30:51 -0500

    Adding headers

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ app.use( bodyParser.json() )
 app.use( '/uploads/images/', express.static( path.join( 'uploads', 'images' ) ) )
 app.use( express.static( path.join( 'public' ) ) )
 
-// app.use( ( req, res, next ) => {
+app.use( ( req, res, next ) => {
 
-//     res.setHeader( 'Access-Control-Allow-Origin', '*' );
-//     res.setHeader( 'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept, Authorization' )
-//     res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE' )
+    res.setHeader( 'Access-Control-Allow-Origin', '*' );
+    res.setHeader( 'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization' )
+    res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE' )
 
-//     next();
-// } )
+    next();
+} )
 
 
 app.use( "/api/places", placesRoutes )
